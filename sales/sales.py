160800f60025cdb63e8dea853e5c6d347b494430
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-02-15 19:45:29 +0100

    Added function that writes changes to the file in sales module

--- sales/sales.py
+++ sales/sales.py
@@ -58,15 +58,15 @@ def start_module():
                 continue
             elif option == "2":
                 add(table)
-                # data_manager.write_table_to_file(file_name, table)
+                data_manager.write_table_to_file(file_name, table)
                 continue
             elif option == "3":
                 remove(table, id_=ui.get_inputs(["Please enter: "], title_list[ID]))
-                # data_manager.write_table_to_file(file_name, table)
+                data_manager.write_table_to_file(file_name, table)
                 continue
             elif option == "4":
                 update(table, id_=ui.get_inputs(["Please enter: "], title_list[ID]))
-                # data_manager.write_table_to_file(file_name, table)
+                data_manager.write_table_to_file(file_name, table)
                 continue
             elif option == "5":
                 get_lowest_price_item_id(table)
